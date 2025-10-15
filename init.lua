8e6edb29a2dc9ffdbbbe5094b04fb023937ee45b
Fernando Cabrera <fcabrera@Fernandos-MacBook-Pro.local>
2025-10-15 07:38:53 -0400

    Themes for yatline

--- init.lua
+++ init.lua
@@ -66,6 +66,20 @@ require("projects"):setup({
 	},
 })
 
+local function detect_dark_mode()
+	local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+	local result = handle:read("*a")
+	handle:close()
+
+	if string.find(result, "Dark") then
+		return true
+	else
+		return false
+	end
+end
+
+local theme
+
 -- Kanagawa Lotus Palette (your tmux colors mapped)
 local lotus = {
 	white_3 = "#DCD7BA", -- light fg
@@ -84,6 +98,49 @@ local lotus = {
 	bg_light = "#545464", -- custom: lighter section bg
 	green_2 = "#C34043", -- red
 }
+
+local dragon = {
+	white_3 = "#DCD7BA", -- light fg
+	true_white = "#181616",
+	gray_3 = "#181616", -- muted gray
+	white_2 = "#393836", -- softer fg
+	violet_1 = "#938aa9", -- subtle accent
+	violet_2 = "#b6927b", -- stronger accent
+	aqua_2 = "#87a987", -- aqua
+	red_3 = "#949fb5", -- green-ish (lotus twist, actually looks reddish)
+	orange_2 = "#8a9a7b", -- green/olive
+	yellow_3 = "#5e857a", -- orange-red
+	pink = "#b98d7b", -- pink accent
+	red_2 = "#5a7785", -- dusty yellow/steel
+	bg_dark = "#54546D", -- custom: dark terminal bg
+	bg_light = "#f2ecbc", -- custom: lighter section bg
+	green_2 = "#8ea4a2", -- red
+}
+
+local wave = {
+	white_3 = "#DCD7BA", -- light fg
+	true_white = "#dcd7ba",
+	gray_3 = "#181616", -- muted gray
+	white_2 = "#2a2a37", -- softer fg
+	violet_1 = "#938aa9", -- subtle accent
+	violet_2 = "#54546D", -- stronger accent
+	aqua_2 = "#98bb6c", -- aqua
+	red_3 = "#949fb5", -- green-ish (lotus twist, actually looks reddish)
+	orange_2 = "#87a987", -- green/olive
+	yellow_3 = "#dca561", -- orange-red
+	pink = "#d9a594", -- pink accent
+	red_2 = "#5a7785", -- dusty yellow/steel
+	bg_dark = "#54546D", -- custom: dark terminal bg
+	bg_light = "#dcd7ba", -- custom: lighter section bg
+	green_2 = "#938aa9", -- red
+}
+
+if detect_dark_mode() then
+	theme = dragon
+	theme = wave
+else
+	theme = lotus
+end
 
 -- ---@class PaletteColors
 -- local palette = {
@@ -214,6 +271,96 @@ local lotus = {
 -- 	lotusCyan = "#d7e3d8",
 -- }
 
+--  fuji_white='#dcd7ba'
+--  sumi_ink_4='#2a2a37'
+--  sumi_ink_2='#1a1a22'
+--  sumi_ink_5='#363646'
+--  sumi_ink_6='#54546D'
+--  wave_aqua='#6a9589'
+--  ronin_yellow='#ff9e3b'
+--  dragon_green='#8a9a7b'
+--  dragon_aqua='#8ea4a2',
+--  autumn_orange='#dca561'
+--  wawe_red='#e46876'
+--  sakura_pink='#d27e99'
+--  ronin_yellow='#ff9e3b'
+--  spring_violet_1='#938aa9'
+--  crystal_blue='#7e9cd8'
+--  wave_blue_2='#2d4f67'
+--  wave_blue_1='#223249'
+--  wave_red='#e46876'
+--  spring_violet_2='#9cabca'
+--  wave_aqua_2='#7aa89f'
+--  carp_yellow='#e6c384'
+--  winter_blue='#252535'
+--  surimi_orange='#ffa066'
+--  spring_green='#98bb6c'
+--  katana_gray='#717c7c'
+--  spring_blue='#7fb4ca'
+--  oni_violet='#957fb8'
+--  autumn_yellow='#dca561'
+--  winter_yellow='#49443c'
+--  winter_red='#43242b'
+--  fuji_gray='#727169'
+--  light_blue='#a3d4d5'
+--  old_white='#c8c093'
+--  samurai_red='#e82424'
+--  sumi_ink_1='#1e1f28'
+--  sumi_ink_0='#16161d'
+--  peach_red='#ff5d62'
+--  sumi_ink_3='#363646'
+--  winter_green='#2b3328'
+--  boat_yellow_2='#c0a36e'
+--  dragon_blue='#658594'
+--  boat_yellow_1='#938056'
+--  autumn_green='#76946a'
+--  autumn_red='#c34043'
+--  dragon_orange='#b6927b'
+--  dragon_gray='#a6a69c'
+--  lotus_pink='#b35b79'
+--  lotus_cyan='#d7e3d8'
+--  lotus_orange='#cc6d00'
+--  lotus_yellow='#77713f'
+--  dragon_red='#c4746e'
+--  dragon_pink='#a292a3'
+--  lotus_white_3='#f2ecbc'
+--  lotus_ink_1='#545464'
+--  lotus_ink_2='#43436c'
+--  lotus_red_2='#d7474b'
+--  lotus_yellow_2='#836f4a'
+--  lotus_teal_2='#6693bf'
+--  lotus_gray_3='#8a8980'
+--  lotus_violet_1='#a09cac'
+--  lotus_violet_2='#766b90'
+--  lotus_orange_2='#e98a00'
+--  lotus_yellow_3='#de9800'
+--  lotus_gray_2='#716e61'
+-- dragon_white='#c5c9c5'
+-- dragon_green='#87a987'
+-- dragon_orange_2='#b98d7b'
+-- dragon_gray_2='#9e9b93'
+-- dragon_gray_3='#7a8382'
+-- dragon_blue_2='#8ba4b0'
+-- dragon_violet='#8992a7'
+-- dragon_aqua='#8ea4a2'
+-- dragon_ash='#737c73'
+-- dragon_teal='#949fb5'
+-- dragon_yellow='#c4b28a'
+-- dragon_black_0='#0d0c0c'
+-- dragon_black_1='#12120f'
+-- dragon_black_2='#1D1C19'
+-- dragon_black_3='#181616'
+-- dragon_black_4='#282727'
+-- dragon_black_5='#393836'
+-- dragon_black_6='#625e5a'
+-- lotus_yellow_4='#f9d791'
+-- lotus_red='#c84053'
+-- lotus_red_4='#d9a594'
+-- lotus_aqua='#597b75'
+-- lotus_aqua_2='#5e857a'
+-- lotus_teal_1='#4e8ca2'
+-- lotus_teal_3='#5a7785'
+
 require("yatline"):setup({
 	section_separators = { left = "", right = "" },
 	section_separator = { close = "", open = "" },
@@ -221,34 +368,34 @@ require("yatline"):setup({
 	inverse_separator = { open = "", close = "" },
 
 	style_a = {
-		fg = lotus.white_2,
+		fg = theme.white_2,
 		bg_mode = {
-			normal = lotus.green_2,
-			select = lotus.red_2,
-			un_set = "#938056",
+			normal = theme.green_2,
+			select = theme.red_2,
+			un_set = theme.gray_3,
 		},
 	},
-	style_b = { bg = lotus.violet_2, fg = lotus.true_white },
-	style_c = { bg = lotus.white_2, fg = lotus.bg_light },
+	style_b = { bg = theme.violet_2, fg = theme.true_white },
+	style_c = { bg = theme.white_2, fg = theme.bg_light },
 
-	permissions_t_fg = lotus.orange_2,
-	permissions_r_fg = lotus.red_2,
-	permissions_w_fg = lotus.red_3,
-	permissions_x_fg = lotus.orange_2,
-	permissions_s_fg = lotus.pink,
+	permissions_t_fg = theme.orange_2,
+	permissions_r_fg = theme.red_2,
+	permissions_w_fg = theme.red_3,
+	permissions_x_fg = theme.orange_2,
+	permissions_s_fg = theme.pink,
 
 	tab_width = 20,
 	tab_use_inverse = false,
 
-	selected = { icon = "󰻭", fg = lotus.yellow_3 },
-	copied = { icon = "", fg = lotus.aqua_2 },
-	cut = { icon = "", fg = lotus.red_3 },
+	selected = { icon = "󰻭", fg = theme.yellow_3 },
+	copied = { icon = "", fg = theme.aqua_2 },
+	cut = { icon = "", fg = theme.red_3 },
 
-	total = { icon = "󰮍", fg = lotus.yellow_3 },
-	succ = { icon = "", fg = lotus.aqua_2 },
-	fail = { icon = "", fg = lotus.red_2 },
-	found = { icon = "󰮕", fg = lotus.violet_1 },
-	processed = { icon = "󰐍", fg = lotus.green_2 },
+	total = { icon = "󰮍", fg = theme.yellow_3 },
+	succ = { icon = "", fg = theme.aqua_2 },
+	fail = { icon = "", fg = theme.red_2 },
+	found = { icon = "󰮕", fg = theme.violet_1 },
+	processed = { icon = "󰐍", fg = theme.green_2 },
 
 	show_background = true,
 
